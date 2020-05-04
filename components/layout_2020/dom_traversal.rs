0aaa4cd6fca9c326496c975020496a64be77c00d
Simon Sapin <simon.sapin@exyr.org>
2020-05-04 17:25:52 +0200

    Fix panics for unwrapping `None` layout data in `unset_boxes_in_subtree`

--- components/layout_2020/dom_traversal.rs
+++ components/layout_2020/dom_traversal.rs
@@ -117,7 +117,8 @@ fn traverse_element<'dom, Node>(
                 // <https://drafts.csswg.org/css-display-3/#valdef-display-contents>
                 element.unset_boxes_in_subtree()
             } else {
-                *element.layout_data_mut().self_box.borrow_mut() = Some(LayoutBox::DisplayContents);
+                *element.layout_data_mut().unwrap().self_box.borrow_mut() =
+                    Some(LayoutBox::DisplayContents);
                 traverse_children_of(element, context, handler)
             }
         },
@@ -369,7 +370,7 @@ pub(crate) trait NodeExt<'dom>: 'dom + Copy + LayoutNode<'dom> + Send + Sync {
     fn style(self, context: &LayoutContext) -> ServoArc<ComputedValues>;
 
     fn as_opaque(self) -> OpaqueNode;
-    fn layout_data_mut(self) -> AtomicRefMut<'dom, LayoutDataForElement>;
+    fn layout_data_mut(self) -> Option<AtomicRefMut<'dom, LayoutDataForElement>>;
     fn element_box_slot(&self) -> BoxSlot<'dom>;
     fn pseudo_element_box_slot(&self, which: WhichPseudoElement) -> BoxSlot<'dom>;
     fn unset_pseudo_element_box(self, which: WhichPseudoElement);
@@ -447,18 +448,17 @@ where
     }
 
     #[allow(unsafe_code)]
-    fn layout_data_mut(self) -> AtomicRefMut<'dom, LayoutDataForElement> {
+    fn layout_data_mut(self) -> Option<AtomicRefMut<'dom, LayoutDataForElement>> {
         self.get_style_and_layout_data()
             .map(|d| d.layout_data.borrow_mut())
-            .unwrap()
     }
 
     fn element_box_slot(&self) -> BoxSlot<'dom> {
-        BoxSlot::new(self.layout_data_mut().self_box.clone())
+        BoxSlot::new(self.layout_data_mut().unwrap().self_box.clone())
     }
 
     fn pseudo_element_box_slot(&self, which: WhichPseudoElement) -> BoxSlot<'dom> {
-        let mut data = self.layout_data_mut();
+        let mut data = self.layout_data_mut().unwrap();
         let pseudos = data.pseudo_elements.get_or_insert_with(Default::default);
         let cell = match which {
             WhichPseudoElement::Before => &mut pseudos.before,
@@ -468,7 +468,7 @@ where
     }
 
     fn unset_pseudo_element_box(self, which: WhichPseudoElement) {
-        if let Some(pseudos) = &mut self.layout_data_mut().pseudo_elements {
+        if let Some(pseudos) = &mut self.layout_data_mut().unwrap().pseudo_elements {
             match which {
                 WhichPseudoElement::Before => *pseudos.before.borrow_mut() = None,
                 WhichPseudoElement::After => *pseudos.after.borrow_mut() = None,
@@ -483,11 +483,12 @@ where
         let mut node = self;
         loop {
             if node.is_element() {
-                let traverse_children = {
-                    let mut layout_data = node.layout_data_mut();
+                let traverse_children = if let Some(mut layout_data) = node.layout_data_mut() {
                     layout_data.pseudo_elements = None;
                     let self_box = layout_data.self_box.borrow_mut().take();
                     self_box.is_some()
+                } else {
+                    false
                 };
                 if traverse_children {
                     // Only descend into children if we removed a box.
